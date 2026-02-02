0550a709eada1c446e64d897d2c87b49edfe42ea
risusolutions-hub <risusolutions@gmail.com>
2026-02-02 22:14:59 +0530

    url chnage

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://sp.vruti.in/api',
+  baseURL: 'https://finel-sl.onrender.com/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' }
 });
